8a42dde0e545cf860b0dd5803cc0b88024ab0775
etherioush <dbguswns147@naver.com>
2023-11-18 00:12:56 +0900

    Add files via upload

--- js/typing.js
+++ js/typing.js
@@ -1,23 +1,44 @@
-const content = "안녕하세요! \n";
-const text = document.querySelector("#typ1");
-const content1 = "신입 백엔드 개발자 유현준입니다.";
-const text1 = document.querySelector("#typ2");
-let i = 0;
-let j = 0;
+$(document).ready(function(){
+    var typingBool = false; 
+    var typingIdx=0; 
+    var liIndex = 0;
+    var liLength = $(".typing-txt>ul>li").length;
 
-function typing() {
-    if (i < content.length) {
-        let txt = content[i++];
-        text.innerHTML += txt === "\n" ? "<br/>" : txt;
-    } else if (j < content1.length) {
-        let txt = content1[j++];
-        text1.innerHTML += txt === "\n" ? "<br/>" : txt;
-    }
-    if (i > content.length && j > content1.length) {
-        text.textContent = "";
-        text1.textContent = "";
-        i = 0;
-        j = 0;
-    }
-}
-setInterval(typing, 140);
+    // 타이핑될 텍스트를 가져온다 
+    var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+        typingBool=true; 
+        var tyInt = setInterval(typing,100); // 반복동작 
+    } 
+        
+    function typing(){ 
+    $(".typing ul li").removeClass("on");
+    $(".typing ul li").eq(liIndex).addClass("on");
+    if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+        $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+        typingIdx++; 
+    } else{ if(liIndex<liLength-1){
+        //다음문장으로  가기위해 인덱스를 1증가
+        liIndex++; 
+        //다음문장을 타이핑하기위한 셋팅
+            typingIdx=0;
+            typingBool = false; 
+            typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+        
+        //다음문장 타이핑전 1초 쉰다
+            clearInterval(tyInt);
+            //타이핑종료
+        
+            setTimeout(function(){
+            //1초후에 다시 타이핑 반복 시작
+            tyInt = setInterval(typing,100);
+            },1000);
+            } else if(liIndex==liLength-1){
+            
+            //마지막 문장까지 써지면 반복종료
+            clearInterval(tyInt);
+            }
+        } 
+    }  
+});
